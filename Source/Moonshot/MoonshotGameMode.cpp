c9c9d848477a50435a6c2af0fa80204c37e3a548
toilari <ilari97@hotmail.com>
2020-11-02 20:27:59 +0200

    dev: initial commit

---
+++ Source/Moonshot/MoonshotGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoonshotGameMode.h"
+#include "MoonshotHUD.h"
+#include "MoonshotCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMoonshotGameMode::AMoonshotGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AMoonshotHUD::StaticClass();
+}
---
+++ Source/Moonshot/MoonshotHUD.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoonshotHUD.h"
+#include "Engine/Canvas.h"
+#include "Engine/Texture2D.h"
+#include "TextureResource.h"
+#include "CanvasItem.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMoonshotHUD::AMoonshotHUD()
+{
+	// Set the crosshair texture
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	CrosshairTex = CrosshairTexObj.Object;
+}
+
+
+void AMoonshotHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	// Draw very simple crosshair
+
+	// find center of the Canvas
+	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+	const FVector2D CrosshairDrawPosition( (Center.X - CrosshairTex->GetSurfaceWidth() / 2.0f),
+										   (Center.Y - CrosshairTex->GetSurfaceHeight() / 2.0f));
+
+	// draw the crosshair
+	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	TileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem( TileItem );
+}
