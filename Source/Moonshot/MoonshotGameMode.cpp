594a6729709e2264df164eeabd0796c067a9fa73
toilari <ilari97@hotmail.com>
2020-11-02 20:50:26 +0200

    dev: testing Git integration

--- Source/Moonshot/MoonshotGameMode.cpp
+++ Source/Moonshot/MoonshotGameMode.cpp
@@ -9,7 +9,7 @@ AMoonshotGameMode::AMoonshotGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Moonshot/Characters/BP_PlayerCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
